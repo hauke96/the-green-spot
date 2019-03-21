c4a941be1580e9fe3e99b734f24ac222533d9c08
Hauke Stieler <mail@hauke-stieler.de>
2019-03-21 17:54:50 +0100

    Start firefox in background

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 URL=$(cat config.toml | grep "^baseurl = " | grep -oE "\"(.*)\"" | sed "s/\"\(.*\)\"/\1/")
 echo "Use URL='$URL'"
 
-firefox --new-window "$URL"
+firefox --new-window "$URL" &
 
 hugo serve -D -v
-
