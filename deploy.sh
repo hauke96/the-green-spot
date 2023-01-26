b4d5102d227e6b8c04136ab0147490fb02ab6d75
Hauke Stieler <mail@hauke-stieler.de>
2023-01-26 19:29:23 +0100

    Fix bug in deploy script

--- deploy.sh
+++ deploy.sh
@@ -26,18 +26,18 @@ else
 	else
 		./build.sh
 	fi
-	
-	hline "2. Build package"
-	
-	date_str=$(date -u +"%y-%m-%d_%H-%M-%S")
-	file="the-green-spot_$date_str.zip"
-	
-	cd public 
-	zip -r -9 ../$file ./*
-	cd ..
-	echo
-	ls -alh $file
 fi
+
+hline "2. Build package"
+
+date_str=$(date -u +"%y-%m-%d_%H-%M-%S")
+file="the-green-spot_$date_str.zip"
+
+cd public 
+zip -r -9 ../$file ./*
+cd ..
+echo
+ls -alh $file
 
 hline "3. Login into server"
 
@@ -49,6 +49,8 @@ read -s password
 echo
 
 hline "4. Upload package"
+
+echo "Upload file $file"
 
 sshpass -p $password scp -oHostKeyAlgorithms=+ssh-rsa -r $file $username@the-green-spot.de:$home/
 
